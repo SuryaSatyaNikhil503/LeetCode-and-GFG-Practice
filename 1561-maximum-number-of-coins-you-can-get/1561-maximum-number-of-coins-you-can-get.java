a7d0255a9930ef14edbb4e5e4ff45fa1b8704232
Surya Satya Nikhil <102517041+SuryaSatyaNikhil503@users.noreply.github.com>
2023-11-24 19:24:20 +0530

    Time: 28 ms (40.74%), Space: 54.8 MB (55.20%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int maxCoins(int[] piles) 
+    {
+        Arrays.sort(piles);
+        
+        int n = piles.length / 3;
+        
+        int ans = 0;
+        
+        for (int i = piles.length - 2; i >= n; i -= 2) 
+        {
+           ans += piles[i];
+        }
+        
+        return ans;
+    }
+}
